a2a08d79b26e706e21f12baf8fa9c2da1dbb86c1
vinicius-gandini <viniciusgandini.goncalves@hotmail.com>
2020-07-18 22:11:39 -0300

    CRUD finished :ok_hand:

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -53,7 +53,36 @@ class UserController {
 
       return res.json(novosDados);
     } catch (e) {
-      return res.json(null);
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ["ID não enviado"],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ["Usuário não existe"],
+        });
+      }
+
+      await user.destroy();
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
     }
   }
 }
