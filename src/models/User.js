5a160e2bf4e6d33138be4ecb397b312ac5457a38
vinicius-gandini <viniciusgandini.goncalves@hotmail.com>
2020-07-18 21:51:07 -0300

    Begin CRUD with list users, all and especific, and update route :ok_hand:

--- src/models/User.js
+++ src/models/User.js
@@ -49,7 +49,9 @@ export default class User extends Model {
     );
 
     this.addHook("beforeSave", async (user) => {
-      user.password_hash = await bcryptjs.hash(user.password, 8);
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
     });
 
     return this;
