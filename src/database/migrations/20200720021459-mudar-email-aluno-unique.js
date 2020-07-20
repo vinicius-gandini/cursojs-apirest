9215060192f7ca0805193f75ac1ef185465b2aed
vinicius-gandini <viniciusgandini.goncalves@hotmail.com>
2020-07-20 00:17:05 -0300

    Created route Aluno to init CRUD :ok_hand:

---
+++ src/database/migrations/20200720021459-mudar-email-aluno-unique.js
@@ -0,0 +1,10 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) =>
+    queryInterface.changeColumn("alunos", "email", {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    }),
+
+  down: async () => {},
+};
