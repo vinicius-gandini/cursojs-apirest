dd9211538f0e561224a8d695a7667402beb0a694
vinicius-gandini <viniciusgandini.goncalves@hotmail.com>
2020-07-21 00:19:02 -0300

    Created complete CRUD of Alunos with JWT too :ok_hand:

--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -4,12 +4,65 @@ export default class Aluno extends Model {
   static init(sequelize) {
     super.init(
       {
-        nome: Sequelize.STRING,
-        sobrenome: Sequelize.STRING,
-        email: Sequelize.STRING,
-        idade: Sequelize.INTEGER,
-        peso: Sequelize.FLOAT,
-        altura: Sequelize.FLOAT,
+        nome: {
+          type: Sequelize.STRING,
+          defaultValue: "",
+          validate: {
+            len: {
+              args: [3, 25],
+              msg: "Nome precisa conter entre 3 e 25 caracteres",
+            },
+          },
+        },
+        sobrenome: {
+          type: Sequelize.STRING,
+          defaultValue: "",
+          validate: {
+            len: {
+              args: [3, 25],
+              msg: "Sobrenome precisa conter entre 3 e 25 caracteres",
+            },
+          },
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: "",
+          unique: {
+            msg: "Email já cadastrado",
+          },
+          validate: {
+            isEmail: {
+              msg: "Email inválido",
+            },
+          },
+        },
+        idade: {
+          type: Sequelize.INTEGER,
+          defaultValue: "",
+          validate: {
+            isInt: {
+              msg: "Idade precisa ser um valor numérico",
+            },
+          },
+        },
+        peso: {
+          type: Sequelize.FLOAT,
+          defaultValue: "",
+          validate: {
+            isFloat: {
+              msg: "Altura precisa ser um valor inteiro ou ponto flutuante",
+            },
+          },
+        },
+        altura: {
+          type: Sequelize.FLOAT,
+          defaultValue: "",
+          validate: {
+            isFloat: {
+              msg: "Altura precisa ser um valor inteiro ou ponto flutuante",
+            },
+          },
+        },
       },
       {
         sequelize,
