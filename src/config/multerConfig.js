a155271f55d53c64552e1a0058c20f12d0514d33
vinicius-gandini <viniciusgandini.goncalves@hotmail.com>
2020-07-21 23:52:02 -0300

    Add rule of only images archives can be uploaded :ok_hand:

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -1,15 +1,25 @@
-import multer from "multer";
-import { extname, resolve } from "path";
+import multer from "multer"; // importar instalação do multer
+import { extname, resolve } from "path"; // importar ath para utilziar os diretórios
 
 const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== "image/png" && file.mimetype !== "image/jpg") {
+      return cb(new multer.MulterError("Arquivo precisa ser .png ou .jpg"));
+    }
+
+    return cb(null, true);
+  },
+  // Confgiruação para utilizar o armazenamento do servidor
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
+      // Define o destino aonde o arquivo será enviado
       cb(null, resolve(__dirname, "..", "..", "uploads"));
     },
     filename: (req, file, cb) => {
-      cb(null, `${Date.now()}_${aleatorio()}${extname(file.originalname)}`);
+      // Trata o nome do arquivo para evitar duplicidade
+      cb(null, `${Date.now()}_${aleatorio()}${extname(file.originalname)}`); // Data e milisegundos + aleatório + extensão original do arquivo
     },
   }),
 };
--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,13 +1,10 @@
 import { Router } from "express";
-import multer from "multer";
 
 import fotoController from "../controllers/FotoController";
-import multerConfig from "../config/multerConfig";
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post("/", upload.single("foto"), fotoController.store);
+// upload.single() define que apenas um arquivo é enviado por vez, com a "id" especificada
+router.post("/", fotoController.store);
 
 export default router;
